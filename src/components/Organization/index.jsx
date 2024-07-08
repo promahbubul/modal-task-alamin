98b047d760801af91242a9e3f687bd493cffc7e5
promahbubul <mahbublalam500@gmail.com>
2024-07-08 16:46:26 +0600

    Update organization

--- src/components/Organization/index.jsx
+++ src/components/Organization/index.jsx
@@ -1,6 +1,28 @@
+import FieldGroup from "../FieldGroup";
+
 const Organization = () => {
   return (
-    <div>Organization</div>
-  )
-}
-export default Organization
+    <>
+      <div className=" h-[calc(100%-40.67px)]  scrollbar-hide overflow-auto">
+        <div className="flex flex-col gap-3 mt-4 overflow-auto">
+          <FieldGroup
+            title={"Andrew Cano"}
+            label={"Organization name"}
+            id={"orgname"}
+          />
+          <FieldGroup
+            title={"Add your organization address to your account"}
+            label={"Organization address"}
+            id={"orgaddress"}
+          />
+          <FieldGroup
+            title={"Add your job title to your account"}
+            label={"Job title"}
+            id={"jobtitle"}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+export default Organization;
